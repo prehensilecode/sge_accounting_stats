fc8e25d6e3acf954ec27733bc6895461d4703a18
David Chin <prehensilecode@users.noreply.github.com>
2024-03-19 13:00:42 -0400

    Add median wait time per month-year table

--- sgestats.py
+++ sgestats.py
@@ -380,6 +380,14 @@ def main():
     plt.savefig('wait_time_vs_slots.png')
     plt.savefig('wait_time_vs_slots.pdf')
 
+    # wait time vs month-year; group by month start
+    wt_vs_mthyr_df = sgeacct_df.loc[:, ['submission_time', 'wait_time']].groupby(pd.Grouper(key='submission_time', freq='MS'))['wait_time'].median().reset_index()
+    print(f'wt_vs_mthyr_df.describe() = \n{wt_vs_mthyr_df.describe()}\n\n')
+
+    with open('wait_by_month.html', 'w') as htmlfile:
+        htmlfile.write(wt_vs_mthyr_df.to_html(index=False))
+
+
 if __name__ == '__main__':
     main()
 
