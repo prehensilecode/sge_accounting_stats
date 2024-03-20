50786489e736af6ed16c9f6c1189b5054cb5c56d
David Chin <prehensilecode@users.noreply.github.com>
2024-03-20 16:50:20 -0400

    Remove pseudo users

--- top10.py
+++ top10.py
@@ -5,38 +5,6 @@ from pathlib import Path
 import pandas as pd
 
 
-def prep_accounting(sgeacct_df, debug_p: bool):
-    debug_p = False
-    info_p = False
-    # expand the df with stuff from the "-l resources_list"
-
-    resources = sgeacct_df['category'].apply(parse_categories)
-
-    if info_p:
-        print(f'INFO: prep_accounting: type(resources) = {type(resources)}')
-
-    #resources_df = pd.DataFrame(resources.tolist(), index=resources.index)
-    resources_df = pd.DataFrame.from_records(resources.values, index=resources.index)
-
-    if info_p:
-        print(f'INFO: prep_accounting: sgeacct_df.describe() = \n{sgeacct_df.describe()}')
-        print(f'INFO: prep_accounting: sgeacct_df.columns = \n{sgeacct_df.columns}')
-        print(f'INFO: prep_accounting: sgeacct_df.head() = \n{sgeacct_df.head()}')
-
-        print(f'INFO: prep_accounting: resources_df.describe() = \n{resources_df.describe()}')
-        print(f'INFO: prep_accounting: resources_df.columns = \n{resources_df.columns}')
-        print(f'INFO: prep_accounting: resources_df.head() = \n{resources_df.head()}')
-
-    ret_df = pd.concat([sgeacct_df, resources_df], axis=1)
-
-    if info_p:
-        print( 'INFO: prep_accounting: after pd.concat()')
-        print(f'INFO: prep_accounting: ret_df.columns = \n{ret_df.columns}')
-        print(f'INFO: prep_accounting: ret_df.head() = \n{ret_df.head()}')
-
-    return ret_df
-
-
 def main():
     debug_p = False
 
@@ -55,6 +23,26 @@ def main():
     now = datetime.datetime.now()
     sixmonths = now - datetime.timedelta(days=(6 * 30))
     print(f'DEBUG: sixmonths = {sixmonths}')
+
+    # drop project pseudo users (manual list for now)
+    users_to_drop = set(['niagads', 'nbthetaconn', 'rbc', 'pennlincqsm',
+                         'tumordti', 'hbcddev', 'cardiauser', 'abcdqsiprep',
+                         'publicstudies', 'autismuser', 'luowmdev',
+                         'cbpdmaindata', 'mesa', 'bpdmaindata',
+                         'multisitemd', 'superres', 'socialbrainreconfig',
+                         'dbtai', 'psychanalysis', 'aslprep', 'ipp',
+                         'blsauser', 'accorduser', 'pfnabcd', 'ccbt',
+                         'sprintuser', 'tcgaluaddatareleases',
+                         'ukbbprocessed', 'tmsbp', 'oasis', 'braintumoruser',
+                         'adniuser', 'ukbbdatareleases', 'bbluser', 'rtfmri',
+                         'networkreplication', 'anxiousmisery', 'respondvalidation',
+                         'chistosrubencases', 'sleepplasticity', 'wolfintrinsic',
+                         'whimuser', 'aibl', 'ashpfnsexdiffabcd', 'recover',
+                         'chrisclarkuser', 'preventad', 'biocard', 'lookaheaduser',
+                         'preclinicalad', 'tbi', 'pigtbi', 'braintumorexternal',
+                         'bridgeport', 'calico', ])
+
+    sgeacct_df = sgeacct_df[~sgeacct_df['owner'].isin(users_to_drop)]
 
     lastsixmonths_df = sgeacct_df[sgeacct_df['start_time'] >= sixmonths]
     usagebyuser_df = lastsixmonths_df.loc[:, ['owner', 'cpu']].groupby('owner').sum(numeric_only=True).sort_values(by='cpu', ascending=False).apply(pd.to_timedelta, unit='s').reset_index()
