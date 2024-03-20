d326f4216914eec9e94a274596ed9e83d05821bb
David Chin <prehensilecode@users.noreply.github.com>
2024-03-20 12:37:54 -0400

    Analysis of gpu stats

---
+++ GPU_utilization/gpustats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+gpustats_df = pd.read_csv('gpu_stats.csv')
+gpustats_df.describe()
+
+# shorten some GPU type names
+gpustats_df.loc[gpustats_df['GPU type'] == 'Tesla P100-PCIE-12GB', 'GPU type'] = 'P100'
+gpustats_df.loc[gpustats_df['GPU type'] == 'Tesla V100-PCIE-16GB', 'GPU type'] = 'V100'
+
+bygputype_df = gpustats_df[['GPU type', 'memUsed (MiB)', 'memory percentage used']].groupby('GPU type')
+
+bygputype_df.describe()
